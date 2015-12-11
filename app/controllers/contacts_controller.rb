6b65ee72462c51289c9728c4a9c47c5aaa4fdc7e
Syed Anab Imam <syedanabimam@gmail.com>
2015-12-11 00:12:59 +0000

    Fixed bugs in Mailer files

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,11 +7,11 @@ class ContactsController < ApplicationController
         @contact = Contact.new(contact_params)
         
         if @contact.save
-            name = params[:contact][:name]
-            email = params[:contact][:email]
-            body = params[:contact][:comemnts]
+           name = params[:contact][:name]
+           email = params[:contact][:email]
+           body = params[:contact][:comemnts]
             
-            ContactMailer.contact_email(name, email, body).deliver
+           ContactMailer.contact_email(name, email, body).deliver
            flash[:success] = 'Message sent. Our team will get back to you asap!'
            redirect_to new_contact_path
        else
